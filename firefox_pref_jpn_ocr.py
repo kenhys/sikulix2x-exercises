f0ab577c6a8d5f89c7389ad3935d82e7663304e1
Kentaro Hayashi <kenhys@gmail.com>
2022-10-15 00:15:50 +0900

    Fix a typo

--- firefox_pref_jpn_ocr.py
+++ firefox_pref_jpn_ocr.py
@@ -47,7 +47,7 @@ class FxVerify:
 # Log configuration
 Settings.UserLogs = True
 Settings.UserLogPrefix = "fxverify"
-now = dateTime.datetime.today()
+now = datetime.datetime.today()
 Debug.setUserLogFile("log." + now.strftime("%Y%m%d.%H%M%S") + ".txt")
 
 # OCR configuration
